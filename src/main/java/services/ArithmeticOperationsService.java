02cb32e153a02226a67d6083e6b12f1d564c5ea1
Seirlis <konstantinos.seirlis@vodafone.com>
2018-04-13 14:08:06 +0300

    Initial Commit

---
+++ src/main/java/services/ArithmeticOperationsService.java
@@ -0,0 +1,14 @@
+package services;
+
+public interface ArithmeticOperationsService {
+
+	int multiply(int a, int b);
+	
+	double divide(int a, int b);
+	
+	int add(int a, int b);
+	
+	int minus(int a, int b);
+	
+	
+}
---
+++ src/main/java/services/ArithmeticOperationsServiceTest.java
@@ -0,0 +1,30 @@
+package services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ArithmeticOperationsServiceTest {
+
+	@Test
+	void testMultiply() {
+		int a = 5;
+		int b = 2;
+	}
+
+	@Test
+	void testDivide() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMinus() {
+		fail("Not yet implemented");
+	}
+
+}
