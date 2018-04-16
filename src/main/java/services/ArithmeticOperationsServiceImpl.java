b144e39776ab7f64a155fb7d1a8e08feda3da0c0
Seirlis <konstantinos.seirlis@vodafone.com>
2018-04-16 16:06:28 +0300

    Sum method introduced

---
+++ src/main/java/services/ArithmeticOperationsServiceImpl.java
@@ -0,0 +1,46 @@
+package services;
+
+/**
+ * @author seirlisk
+ *
+ */
+public class ArithmeticOperationsServiceImpl implements ArithmeticOperationsService {
+
+	/**
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public int multiply(int a, int b) {
+		return a*b;
+	}
+	
+	/**
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public double divide(int a, int b) {
+		return 0;
+	}
+	
+	/**
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public int add(int a, int b) {
+		return 0;
+	}
+	
+	/**
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public int minus(int a, int b) {
+		return 0;
+	}
+	
+	
+}
